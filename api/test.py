23c69c4f8bafcb54a84b0c2d9835d32bfd34ca4a
hotteok00 <hojun7889@gmail.com>
2024-06-07 00:31:29 +0900

    api integration in test.py

--- api/test.py
+++ api/test.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import Body, FastAPI
+from fastapi import FastAPI, UploadFile, Body
 
 from PIL import Image
 import io
 
 from small_categorize import predict_image_category_s
 from medium_categorize import predict_image_category_m
+
+from 스타일기반연결코드 import RecommendRequestDto, main
+
+from typing import List
 
 # server on
 app = FastAPI()
@@ -19,14 +24,15 @@ async def categorization(file: UploadFile):
     predicted_category_s = predict_image_category_s(img)
 
     if predicted_category_m == 0:
-        answer['medium'] = 'BOTTOMS_LONG'
+        answer['medium'] = 'BOTTOMS_LONG'    
     elif predicted_category_m == 1:
-        answer['medium'] = 'BOTTOMS_SHORT'
-        
+        answer['medium'] = 'BOTTOMS_SHORT'        
+    
     if predicted_category_s == 0:
-        answer['samll'] = 'BOTTOMS_LONG_DENIM'
+        answer['samll'] = answer['medium'] + '_DENIM'
     elif predicted_category_s == 1:
-        answer['samll'] =  'BOTTOMS_LONG_SWEAT'
+        answer['samll'] = answer['medium'] + '_SWEAT'
+    
         
     answer['fit'] = 'SKINNY'
     answer['gender'] = 'MALE'
@@ -34,5 +40,9 @@ async def categorization(file: UploadFile):
 
     return answer
 
+@app.post("/recommend")
+async def categorization(recommendDto: List[RecommendRequestDto] = Body(...)):
+    response = main(recommendDto[0].temperature, recommendDto)
+    return response
 
 # uvicorn test:app --reload
